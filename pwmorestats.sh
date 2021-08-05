eac239e13ef6c280208e037b80be65b4cbe75040
Darryl Lee <foob-github@darryl.com>
2021-08-04 17:41:24 -0700

    Params for EMAIL and PASSWORD

--- pwmorestats.sh
+++ pwmorestats.sh
@@ -2,8 +2,11 @@ GW="192.168.2.51"
 API="https://$GW/api"
 LOGFILE="/home/pi/workplace/teslog/all.log"
 JAR="/home/pi/workplace/teslog/cookies.txt"
+EMAIL="YOUREMAIL"
+PASSWORD="YOURPASSWORD"
+
 date -Iseconds >> $LOGFILE
-curl -c $JAR -b $JAR -s -k -X POST "$API/login/Basic" -H "Content-Type: application/json" -d '{"username":"customer","password":"YOURPASSWORD","email":"YOUREMAIL"}' > /dev/null
+curl -c $JAR -b $JAR -s -k -X POST "$API/login/Basic" -H "Content-Type: application/json" -d '{"username":"customer","password":"$PASSWORD","email":"$EMAIL"}' > /dev/null
 curl -c $JAR -b $JAR -s -k "$API/sitemaster" >> $LOGFILE
 echo >> $LOGFILE
 curl -c $JAR -b $JAR -s -k "$API/system_status/soe" >> $LOGFILE
